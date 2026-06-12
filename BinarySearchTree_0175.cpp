77726aa6673dcb10136bfbf7942d104a73938009
raffibromabar-web <raffibromabar@gmail.com>
2026-06-12 15:07:44 +0700

    menambahkan fungsi

--- BinarySearchTree_0175.cpp
+++ BinarySearchTree_0175.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 //membuat class node
 class Node{
-    
+    //memberi akses modifier serta mendeklarasi variable dan project node
+public:
+    string info;
+    Node* leftChild;
+    Node* rightChilt;
 }
