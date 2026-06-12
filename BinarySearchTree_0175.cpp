a1e239f1122d93a3c8ad90c4b5c6f81232f66ac2
raffibromabar-web <raffibromabar@gmail.com>
2026-06-12 16:03:09 +0700

    menambahkan int main dan pilihan menu

--- BinarySearchTree_0175.cpp
+++ BinarySearchTree_0175.cpp
@@ -94,4 +94,75 @@ public :
                 preorder(ptr->rightChilt);
             }
         }
+
+        void postorder(Node* ptr){
+
+            if (ROOT == nullptr){
+                cout << "Tree is empty" << endl;
+                return;
+            }
+            if (ptr != nullptr){
+                postorder(ptr->leftChild);
+                postorder(ptr->rightChilt);
+                cout << ptr->info << " ";
+            }
+        }
 };
+
+int main()
+{
+    //deklarasi object x sebagai object BinaryTree
+    BinaryTree x;
+
+    //perulangan while selama bernilai benar
+    while (true)
+    {
+        // membuat menu program dan input pilihan
+        cout << "\nMenu" << endl;
+        cout << "1. implement insert operation" << endl;
+        cout << "2. perform inorder traversal" << endl;
+        cout << "3. perform preorder traversal" << endl;
+        cout << "4. perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        //membuat conditional statement dati input ch
+        switch (ch)
+        {
+            case '1':
+            {
+                cout << "Enter a word : ";
+                string word;
+                cin >> word;
+                x.insert(word);
+                break; 
+            }
+            case '2': {
+                x.inorder(x.ROOT);
+                break;
+            }
+            case '3':{
+                x.preorder(x.ROOT);
+                break;
+            }
+            case '4':{
+                x.postorder(x.ROOT);
+                break;
+            }
+            case '5':{
+                return 0;
+            }
+            default: {
+                cout << "Invalid Option" << endl;
+                break;
+            }
+
+        }
+        
+    }
+}
