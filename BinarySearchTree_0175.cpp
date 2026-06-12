6eecc30ca1aa9ab72451d4cb1dbb87ea4e8b8b35
raffibromabar-web <raffibromabar@gmail.com>
2026-06-12 15:11:40 +0700

    menambahkan nilai parameter ke veriabel

--- BinarySearchTree_0175.cpp
+++ BinarySearchTree_0175.cpp
@@ -11,8 +11,10 @@ public:
 
     
     //membuat constructor untuk class node
-    Node(string i, Node* 1, Node* r){
-        
-        
+    Node(string i, Node* l, Node* r){
+        //memberi nilai parameter ke variabel
+        info = i;
+        leftChild = l;
+        rightChilt = r;
     }
 }
